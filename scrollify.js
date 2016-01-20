099536919f044fcdf676074f09d58c48234fb9ee
Stephan de Vries <stephan281094@users.noreply.github.com>
2016-01-20 21:30:58 +0100

    Improve code styling and add jquery caching

--- scrollify.js
+++ scrollify.js
@@ -1,23 +1,24 @@
+var $ = window.$;
+
 /* http://github.com/stephan281094/scrollify.js
  v1.0 | 20140522
  License: none (public domain)
  */
-(function() {
-  $.fn.scrollify = function(parentElement) {
-    if (parentElement == null) {
-      parentElement = "body";
-    }
-    return $(this).hover(function() {
-      if ($(this).get(0).scrollHeight > $(this).get(0).clientHeight) {
-        return $(parentElement).css({
-          "overflow-y": "hidden"
-        });
-      }
-    }, function() {
-      return $(parentElement).css({
-        "overflow-y": "scroll"
-      });
-    });
-  };
+(function () {
+  $.fn.scrollify = function (parentElement) {
+    var $scope = $(this)
+    var $parentElement = parentElement == null ? 'body' : parentElement
 
-}).call(this);
+    return $scope.hover(function () {
+      if ($scope.get(0).scrollHeight > $scope.get(0).clientHeight) {
+        return $parentElement.css({
+          'overflow-y': 'hidden'
+        })
+      }
+    }, function () {
+      return $parentElement.css({
+        'overflow-y': 'scroll'
+      })
+    })
+  }
+}).call(this)
